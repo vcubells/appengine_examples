ad2daf88159ea09229d4cc0d310c0da5c851e948
Vicente Cubells <vcubells@gmail.com>
2019-02-14 17:35:31 -0600

    Se adicionó el ejemplo en Flexible Env

---
+++ flexible/flask-redis-mongodb/config.py
@@ -0,0 +1,17 @@
+# --- On cloud resources ---
+# Mongo configuration
+MONGO_URI = 'connectionString'
+
+# Redis configuration
+REDIS_HOST = 'connectionString'
+REDIS_PORT = port
+REDIS_PASSWORD = 'password'
+
+# --- Localhost resources ---
+# # Mongo configuration
+# MONGO_URI = 'mongodb://localhost:27017/notes'
+#
+# # Redis configuration
+# REDIS_HOST = 'localhost'
+# REDIS_PORT = 6379
+# REDIS_PASSWORD = ''
