5fa074ea3db950bd6572b5b00e5f654cd35b7064
Vicente Cubells <vcubells@gmail.com>
2019-02-14 19:05:17 -0600

    Se cambió la config a env variables

--- standard/flask-redis-mongodb/config.py
+++ standard/flask-redis-mongodb/config.py
@@ -1,17 +1,13 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
 # --- On cloud resources ---
 # Mongo configuration
-MONGO_URI = 'connectionString'
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 
 # Redis configuration
-REDIS_HOST = 'connectionString'
-REDIS_PORT = port
-REDIS_PASSWORD = 'password'
-
-# --- Localhost resources ---
-# # Mongo configuration
-# MONGO_URI = 'mongodb://localhost:27017/notes'
-#
-# # Redis configuration
-# REDIS_HOST = 'localhost'
-# REDIS_PORT = 6379
-# REDIS_PASSWORD = ''
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", "")
